78031c44a7cfd90a3a0ba3577f9746d29d6b8567
Bingo Yang <yangyingbin0107@163.com>
2019-06-22 14:15:05 +0800

    reentrantReadWriteLock 逻辑图

---
+++ concurrent/thread-simple-demo/ReentrantLock_Logic_Graph.md
@@ -0,0 +1,4 @@
+#### 逻辑图画出ReentrantReadWriteLock的实现原理
+
+![ReentrantReadWriteLock](imgs/ReentrantReadWriteLock_Logic_Img.jpg)
+
--- concurrent/thread-simple-demo/src/main/java/reentrantlock/BlockQueueDemo.java
+++ concurrent/thread-simple-demo/src/main/java/reentrantlock/BlockQueueDemo.java
@@ -14,17 +14,23 @@ public class BlockQueueDemo {
     public static void main(String[] args) throws InterruptedException {
         BlockQueue<String> queue = new BlockQueue<>(3);
         String[] strs = new String[]{"Hello World", "Good Morning", "Good Afternoon", "Good Evening", "Good Night"};
-
-        new Thread(() -> {
-            String str = queue.take();
-            System.out.println(String.format("Thread[%s] take %s success", Thread.currentThread().getId(), str));
-        }).start();
-        TimeUnit.SECONDS.sleep(3);
-        new Thread(() -> {
-            for (int i = 0; i < strs.length; i++) {
-                boolean rs = queue.put(strs[i]);
-                System.out.println(String.format("Thread[%s] put rs[%s] %s", Thread.currentThread().getId(), strs[i], rs));
-            }
-        }).start();
+        System.out.println(1>>>16);
+        System.out.println(65536>>>16);
+        System.out.println(131072>>>16);
+        System.out.println(1<<16);
+        System.out.println(1 & ((1 << 16) - 1));
+        System.out.println(65536 & ((1 << 16) - 1));
+        System.out.println(65536+65536);
+//        new Thread(() -> {
+//            String str = queue.take();
+//            System.out.println(String.format("Thread[%s] take %s success", Thread.currentThread().getId(), str));
+//        }).start();
+//        TimeUnit.SECONDS.sleep(3);
+//        new Thread(() -> {
+//            for (int i = 0; i < strs.length; i++) {
+//                boolean rs = queue.put(strs[i]);
+//                System.out.println(String.format("Thread[%s] put rs[%s] %s", Thread.currentThread().getId(), strs[i], rs));
+//            }
+//        }).start();
     }
 }
